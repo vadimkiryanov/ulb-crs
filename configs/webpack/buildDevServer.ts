430966df86eebb35f70b27580f27c303db9d9e50
Vadim Kiryanov <vadim.kiryanov99@mail.ru>
2024-11-16 21:09:14 +0600

    1.6 - Настроили react-router-dom

--- configs/webpack/buildDevServer.ts
+++ configs/webpack/buildDevServer.ts
@@ -3,7 +3,11 @@ import type { Configuration as DevServerConfiguration } from "webpack-dev-server
 
 export function buildDevServer(options: IBuildOptions): DevServerConfiguration {
 	return {
-    port: options.port,
-    open: true,
-  };
+		// Порт, на котором будет запущен dev-сервер
+		port: options.port,
+		// Автоматически открывать браузер при запуске сервера
+		open: true,
+		// Перенаправление всех запросов к корневому файлу (index.html) для поддержки истории API
+		historyApiFallback: true,
+	};
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
-import { render } from "react-dom";
 import { App } from "./App";
+import { createRoot } from "react-dom/client";
 
 import "./index.scss";
+import { BrowserRouter } from "react-router-dom";
 
-render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+
+root.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>
+);
