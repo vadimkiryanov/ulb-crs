71b596e8b6ba6ac0aa30706bd19d19699a22c1f8
Vadim Kiryanov <vadim.kiryanov99@mail.ru>
2024-11-16 21:29:17 +0600

    1.6.2 - Тест ленивой загрузки функции

--- pages/AboutPage/AboutPage.tsx
+++ pages/AboutPage/AboutPage.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { testF } from "./testF";
 
 const AboutPage = () => {
-	return <div>AboutPage</div>;
+	const onClick = async () => {
+		// Ленивая загрузка функции testF, которая выводит "HELLO" в консоль
+		const { testF } = await import("./testF");
+		testF();
+	};
+	return (
+		<>
+			<button onClick={onClick}>hello console</button>
+			<div>AboutPage</div>
+		</>
+	);
 };
 
 export default AboutPage;
