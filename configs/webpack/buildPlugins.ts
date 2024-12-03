592d43a2354c8d8cb2c880e642b91b741a78f834
Vadim Kiryanov <vadim.kiryanov99@mail.ru>
2024-12-03 19:36:33 +0600

    3.2 - Добавлен webpack-bundle-analyzer для анализа бандла

--- configs/webpack/buildPlugins.ts
+++ configs/webpack/buildPlugins.ts
@@ -2,8 +2,8 @@ import HtmlWebpackPlugin from "html-webpack-plugin";
 import webpack from "webpack";
 import { IBuildOptions } from "./types/config";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
-import ReactRefreshWebpackPlugin  from'@pmmmwh/react-refresh-webpack-plugin';
-
+import ReactRefreshWebpackPlugin from "@pmmmwh/react-refresh-webpack-plugin";
+import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 
 export function buildPlugins({ paths, isDev }: IBuildOptions): webpack.WebpackPluginInstance[] {
 	return [
@@ -21,9 +21,19 @@ export function buildPlugins({ paths, isDev }: IBuildOptions): webpack.WebpackPl
 			chunkFilename: "css/[name].[contenthash:8].css", // Для асинхронных файлов, при ленивой загрузке
 		}),
 		new webpack.DefinePlugin({
+			// Определяем переменную __IS_DEV__ для использования в коде (например, для проверки режима разработки)
 			__IS_DEV__: JSON.stringify(isDev),
 		}),
-    new ReactRefreshWebpackPlugin(),
+
+		// Плагин для поддержки горячей перезагрузки компонентов React
+		new ReactRefreshWebpackPlugin(),
+
+		// Плагин для поддержки горячей замены модулей (HMR)
 		new webpack.HotModuleReplacementPlugin(),
+
+		// Плагин для анализа бандлов и их размера
+		new BundleAnalyzerPlugin({
+			openAnalyzer: false, // отключаем автоматическое открытие анализатора
+		}),
 	];
 }
